df68763873f47e2341e7e7868c77700dbef3f87d
TimothyWng <wng.timothy@gmail.com>
2023-04-06 05:28:50 -0500

    Blanks done, animations still broken

--- bot.py
+++ bot.py
@@ -36,76 +36,76 @@ passSearch.send_keys(Keys.RETURN)
 time.sleep(5) # wait for sign in
 
 
-# IN WEBSITE
-# check 2x boxes
-print('Starting Animations...')
-speedChecks = driver.find_elements(By.CLASS_NAME, "speed-control")
-totChecks = 0
-for check in speedChecks:
-    check.click()
-    button = driver.find_element(By.XPATH, '//button[normalize-space()="Start"]')
-    print('Clicking 2x check')
-    button.click()
-    totChecks += 1
-    time.sleep(0.1)
+# # IN WEBSITE
+# # check 2x boxes
+# print('Starting Animations...')
+# speedChecks = driver.find_elements(By.CLASS_NAME, "speed-control")
+# totChecks = 0
+# for check in speedChecks:
+#     check.click()
+#     button = driver.find_element(By.XPATH, '//button[normalize-space()="Start"]')
+#     print('Clicking 2x check')
+#     button.click()
+#     totChecks += 1
+#     time.sleep(0.1)
 
-animationControl = driver.find_elements(By.CLASS_NAME, 'animation-controls')
-print("ANIMATION CONTROLS:", animationControl)
-#SCROLL TO TOP
-desired_y = (animationControl[0].size['height'] / 2) + animationControl[0].location['y']
-window_h = driver.execute_script('return window.innerHeight')
-window_y = driver.execute_script('return window.pageYOffset')
-current_y = (window_h / 2) + window_y
-scroll_y_by = desired_y - current_y
+# animationControl = driver.find_elements(By.CLASS_NAME, 'animation-controls')
+# print("ANIMATION CONTROLS:", animationControl)
+# #SCROLL TO TOP
+# desired_y = (animationControl[0].size['height'] / 2) + animationControl[0].location['y']
+# window_h = driver.execute_script('return window.innerHeight')
+# window_y = driver.execute_script('return window.pageYOffset')
+# current_y = (window_h / 2) + window_y
+# scroll_y_by = desired_y - current_y
 
-time.sleep(0.1)
-done = 0
-ndx = 1
-numActiveAnimations = len(animationControl)
-animationDone = False
-while animationDone == False:
-    for i in range(numActiveAnimations):
-        print("Scrolling to animation...")
-        # scroll to button
-        desired_y = (animationControl[i].size['height'] / 2) + animationControl[i].location['y']
-        window_h = driver.execute_script('return window.innerHeight')
-        window_y = driver.execute_script('return window.pageYOffset')
-        current_y = (window_h / 2) + window_y
-        scroll_y_by = desired_y - current_y
-        time.sleep(0.5) # 1 sec grace period before checking
-        # accesses button
-        buttonStatus = animationControl[i].find_elements(By.XPATH, "//button[contains(@class, 'normalize-controls')]")
+# time.sleep(0.1)
+# done = 0
+# ndx = 1
+# numActiveAnimations = len(animationControl)
+# animationDone = False
+# while animationDone == False:
+#     for i in range(numActiveAnimations):
+#         print("Scrolling to animation...")
+#         # scroll to button
+#         desired_y = (animationControl[i].size['height'] / 2) + animationControl[i].location['y']
+#         window_h = driver.execute_script('return window.innerHeight')
+#         window_y = driver.execute_script('return window.pageYOffset')
+#         current_y = (window_h / 2) + window_y
+#         scroll_y_by = desired_y - current_y
+#         time.sleep(0.5) # 1 sec grace period before checking
+#         # accesses button
+#         buttonStatus = animationControl[i].find_elements(By.XPATH, "//button[contains(@class, 'normalize-controls')]")
         
-        # gets button status, play, pause, or play again
-        x = buttonStatus[ndx]
-        actions = ActionChains(driver)
-        actions.move_to_element(x).perform()
-        #print("     ndx:",ndx)
-        aria_label = x.get_attribute("aria-label")
-        print("STATUS:", aria_label)
-        if aria_label == 'Play again':
-            numActiveAnimations -= 1
-            print("FINISHED ONE ANIMATION")
-        elif aria_label == 'Play':
-            print('Playing...')
-            x.click()
-        time.sleep(0.5)
-        ndx += 2
+#         # gets button status, play, pause, or play again
+#         x = buttonStatus[ndx]
+#         actions = ActionChains(driver)
+#         actions.move_to_element(x).perform()
+#         #print("     ndx:",ndx)
+#         aria_label = x.get_attribute("aria-label")
+#         print("STATUS:", aria_label)
+#         if aria_label == 'Play again':
+#             numActiveAnimations -= 1
+#             print("FINISHED ONE ANIMATION")
+#         elif aria_label == 'Play':
+#             print('Playing...')
+#             x.click()
+#         time.sleep(0.5)
+#         ndx += 2
 
-        # COUNT HOW MANY DONE
-        finishedCounter = 0
-        for j in buttonStatus:
-            #print('--Attribute', j.get_attribute("aria-label"))
-            if j.get_attribute("aria-label") == 'Play again':
-                finishedCounter += 1
-        # IF DONE
-        if finishedCounter == totChecks or numActiveAnimations == 0:
-            animationDone = True
-            break
-        #print("NUM ACTIVE:", numActiveAnimations)
-    ndx = 1
-print('Finished animations...')
-print()
+#         # COUNT HOW MANY DONE
+#         finishedCounter = 0
+#         for j in buttonStatus:
+#             #print('--Attribute', j.get_attribute("aria-label"))
+#             if j.get_attribute("aria-label") == 'Play again':
+#                 finishedCounter += 1
+#         # IF DONE
+#         if finishedCounter == totChecks or numActiveAnimations == 0:
+#             animationDone = True
+#             break
+#         #print("NUM ACTIVE:", numActiveAnimations)
+#     ndx = 1
+# print('Finished animations...')
+# print()
 
 # FILL IN THE BLANKS
 print('Filling in the blanks...')
@@ -122,14 +122,10 @@ for answer in showAnswerButton:
     # double click
     answer.click()
     answer.click()
-    time.sleep(2)
-
-
 
 answers = driver.find_elements(By.XPATH, "//span[contains(@class, 'forfeit-answer')]")
 textFields = driver.find_elements(By.XPATH, "//textarea[contains(@class, 'ember-text-area')]")
-checkButtons = driver.find_elements(By.XPATH, "//button[contains(@class, 'check-button' and 'zb-button')]")
-print(checkButtons)
+checkButtons = driver.find_elements(By.XPATH, "//button[contains(@class, 'zb-button  primary  raised           check-button')]")
 for x in range(len(answers)):
     # retrieve answer and store
     finalAnswer = answers[x].text
@@ -137,36 +133,11 @@ for x in range(len(answers)):
     # insert answer into textbox
     textFields[x].send_keys(finalAnswer)
     # press CHECK
-
-    time.sleep(0.5)
+    checkButtons[x].click()
 
 
 print('Finished blanks!')
 print()
-
-
-
-totChecks = totChecks + 1
-print()
-
-
-# animations
-# print('Starting animations...')
-# animChecks = driver.find_elements(By.CLASS_NAME, "animation-controls")
-# for a in animChecks:
-#     #button = a.find_element(By.CLASS_NAME, "title")
-#     button = a.find_element(By.XPATH, '//button[normalize-space()="Start"]')
-#     print('Clicking', button.text)
-#     #print("ID:", a.get_attribute("id"))
-#     button.click()
-# print()
-# x = 0
-# while x < totChecks:
-#     if 
-
-# fill in the blanks
-#blanks = driver.find_elements(By.CLASS_NAME, "question-set-question short-answer-question ember-view")
-
 
 # done
 time.sleep(10)
