8c0f1481de643252f72a0b2383f42d20718fe9bf
TimothyWng <wng.timothy@gmail.com>
2023-04-06 05:56:37 -0500

    Ok animations and blanks.

--- bot.py
+++ bot.py
@@ -24,7 +24,6 @@ driver = webdriver.Chrome(service=s, options=chrome_options)
 driver.get("https://learn.zybooks.com/zybook/TAMUCSCE120-121-709Spring2023/chapter/8/section/1")
 print('Opening', driver.title,'...')
 print()
-driver.maximize_window()
 # username
 userSearch = driver.find_element(By.ID, "ember9")
 userSearch.send_keys("timwng@tamu.edu")
@@ -36,76 +35,79 @@ passSearch.send_keys(Keys.RETURN)
 time.sleep(5) # wait for sign in
 
 
-# # IN WEBSITE
-# # check 2x boxes
-# print('Starting Animations...')
-# speedChecks = driver.find_elements(By.CLASS_NAME, "speed-control")
-# totChecks = 0
-# for check in speedChecks:
-#     check.click()
-#     button = driver.find_element(By.XPATH, '//button[normalize-space()="Start"]')
-#     print('Clicking 2x check')
-#     button.click()
-#     totChecks += 1
-#     time.sleep(0.1)
+# IN WEBSITE
+# check 2x boxes
+print('Starting Animations...')
+speedChecks = driver.find_elements(By.CLASS_NAME, "speed-control")
+totChecks = 0
+for check in speedChecks:
+    check.click()
+    button = driver.find_element(By.XPATH, '//button[normalize-space()="Start"]')
+    print('Clicking 2x check')
+    button.click()
+    totChecks += 1
+    time.sleep(0.5)
 
-# animationControl = driver.find_elements(By.CLASS_NAME, 'animation-controls')
-# print("ANIMATION CONTROLS:", animationControl)
-# #SCROLL TO TOP
-# desired_y = (animationControl[0].size['height'] / 2) + animationControl[0].location['y']
-# window_h = driver.execute_script('return window.innerHeight')
-# window_y = driver.execute_script('return window.pageYOffset')
-# current_y = (window_h / 2) + window_y
-# scroll_y_by = desired_y - current_y
+animationControl = driver.find_elements(By.CLASS_NAME, 'animation-controls')
+print("ANIMATION CONTROLS:", animationControl)
+animationDone = False
+#SCROLL TO TOP
+if len(animationControl) > 0:
+    desired_y = (animationControl[0].size['height'] / 2) + animationControl[0].location['y']
+    window_h = driver.execute_script('return window.innerHeight')
+    window_y = driver.execute_script('return window.pageYOffset')
+    current_y = (window_h / 2) + window_y
+    scroll_y_by = desired_y - current_y
+else:
+    animationDone = True
 
-# time.sleep(0.1)
-# done = 0
-# ndx = 1
+done = 0
+ndx = 1
 # numActiveAnimations = len(animationControl)
-# animationDone = False
-# while animationDone == False:
-#     for i in range(numActiveAnimations):
-#         print("Scrolling to animation...")
-#         # scroll to button
-#         desired_y = (animationControl[i].size['height'] / 2) + animationControl[i].location['y']
-#         window_h = driver.execute_script('return window.innerHeight')
-#         window_y = driver.execute_script('return window.pageYOffset')
-#         current_y = (window_h / 2) + window_y
-#         scroll_y_by = desired_y - current_y
-#         time.sleep(0.5) # 1 sec grace period before checking
-#         # accesses button
-#         buttonStatus = animationControl[i].find_elements(By.XPATH, "//button[contains(@class, 'normalize-controls')]")
+while animationDone == False:
+    for i in range(len(animationControl)):
+        print("Scrolling to animation...")
+        # scroll to button
+        desired_y = (animationControl[i].size['height'] / 2) + animationControl[i].location['y']
+        window_h = driver.execute_script('return window.innerHeight')
+        window_y = driver.execute_script('return window.pageYOffset')
+        current_y = (window_h / 2) + window_y
+        scroll_y_by = desired_y - current_y
+        time.sleep(1) # 1 sec grace period before checking
+        # accesses button
+        buttonStatus = animationControl[i].find_elements(By.XPATH, "//button[contains(@class, 'normalize-controls')]")
         
-#         # gets button status, play, pause, or play again
-#         x = buttonStatus[ndx]
-#         actions = ActionChains(driver)
-#         actions.move_to_element(x).perform()
-#         #print("     ndx:",ndx)
-#         aria_label = x.get_attribute("aria-label")
-#         print("STATUS:", aria_label)
-#         if aria_label == 'Play again':
-#             numActiveAnimations -= 1
-#             print("FINISHED ONE ANIMATION")
-#         elif aria_label == 'Play':
-#             print('Playing...')
-#             x.click()
-#         time.sleep(0.5)
-#         ndx += 2
+        # gets button status, play, pause, or play again
+        x = buttonStatus[ndx]
+        actions = ActionChains(driver)
+        actions.move_to_element(x).perform()
+        #print("     ndx:",ndx)
+        aria_label = x.get_attribute("aria-label")
+        print("STATUS:", aria_label)
+        if aria_label == 'Play again':
+            print("FINISHED ONE ANIMATION")
+        elif aria_label == 'Play':
+            print('Playing...')
+            x.click()
+        time.sleep(0.5)
+        ndx += 2
 
-#         # COUNT HOW MANY DONE
-#         finishedCounter = 0
-#         for j in buttonStatus:
-#             #print('--Attribute', j.get_attribute("aria-label"))
-#             if j.get_attribute("aria-label") == 'Play again':
-#                 finishedCounter += 1
-#         # IF DONE
-#         if finishedCounter == totChecks or numActiveAnimations == 0:
-#             animationDone = True
-#             break
-#         #print("NUM ACTIVE:", numActiveAnimations)
-#     ndx = 1
-# print('Finished animations...')
-# print()
+        # COUNT HOW MANY DONE
+        finishedCounter = 0
+        for j in buttonStatus:
+            #print('--Attribute', j.get_attribute("aria-label"))
+            if j.get_attribute("aria-label") == 'Play again':
+                finishedCounter += 1
+        # IF DONE
+        if finishedCounter == totChecks:
+            animationDone = True
+            break
+        finishedCounter = 0
+        #print("NUM ACTIVE:", numActiveAnimations)
+    ndx = 1
+print('Finished animations...')
+time.sleep(1)
+print()
 
 # FILL IN THE BLANKS
 print('Filling in the blanks...')
@@ -140,7 +142,7 @@ print('Finished blanks!')
 print()
 
 # done
-time.sleep(10)
+time.sleep(8)
 print('Closing website...')
 driver.quit()
 
